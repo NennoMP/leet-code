58cea89b9edc2796dde1102eb7194f50e644c0d8
Matteo Pinna <matteo.pinna@hotmail.com>
2022-12-18 23:35:10 +0100

    Add recursive solutions

--- LeetCode/easy/70_climbing_stairs.py
+++ LeetCode/easy/70_climbing_stairs.py
@@ -1,7 +1,9 @@
 # https://leetcode.com/problems/climbing-stairs/
 
+from functools import lru_cache
 
-"Fibonacci numbers based solution."
+
+"Iterative solution."
 class Solution:
     def climbStairs(self, n: int) -> int:
         
@@ -15,3 +17,26 @@ class Solution:
             x2 = result
 
         return result
+
+
+"""Recursive solution with memoization."""
+class Solution:
+    def climbStairs(self, n: int) -> int:
+
+        memo = {0: 1, 1: 1}
+        def climbingStairsRec(n: int, memo: dict) -> int:
+            if n not in memo:
+                memo[n] = climbingStairsRec(n-1, memo) + climbingStairsRec(n-2, memo)
+            return memo[n]
+
+        return climbingStairsRec(n, memo)
+
+
+"""Recursive solution with memoization using @cache decorator."""
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        @lru_cache
+        def climbingStairsRec(n: int) -> int:
+            return 1 if n <= 1 else climbingStairsRec(n-1) + climbingStairsRec(n-2)
+
+        return climbingStairsRec(n)
